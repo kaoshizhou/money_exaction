c5fff26f61173b24abaf9ddd0c6d207a6643b739
yzlin <yzlin@fuzhi.ai>
2022-02-10 16:59:31 +0800

    add submodules

---
+++ update_submodules.sh
@@ -0,0 +1,6 @@
+# # submodule initialization, kept for record
+# git submodule add -b main https://gitlab.deepwisdomai.com/nlp1/products/tianji/nlp_metrics.git
+# git submodule add -b main https://gitlab.deepwisdomai.com/nlp1/autonlpcls/at_template.git
+# git submodule add -b main https://gitlab.deepwisdomai.com/nlp1/products/tianji/nlp_solver.git
+
+git submodule update --remote --merge
